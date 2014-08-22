f25ba3fe85ccb7a7bd49551e79030db76a276604
jesxr44 <jesxr44@gmail.com>
2014-08-23 00:22:42 +0200

    Initial Commit

---
+++ Rodalies/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rodalies
+//
+//  Created by Jesús Escribano on 23/08/14.
+//  Copyright (c) 2014 Running Dogs Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ RodaliesTests/RodaliesTests.m
@@ -0,0 +1,34 @@
+//
+//  RodaliesTests.m
+//  RodaliesTests
+//
+//  Created by Jesús Escribano on 23/08/14.
+//  Copyright (c) 2014 Running Dogs Studio. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RodaliesTests : XCTestCase
+
+@end
+
+@implementation RodaliesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
